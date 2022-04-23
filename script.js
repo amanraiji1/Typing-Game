c620d4c6110ebf1fb559e3eb051fc6ccf7bcab12
Aman Rai <amanraiji1@gmail.com>
2022-04-23 15:51:42 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,234 @@
+const word = document.getElementById("word");
+const text = document.getElementById("text");
+const scoreEl = document.getElementById("score");
+const timeEl = document.getElementById("time");
+const endgameEl = document.getElementById("end-game-container");
+const settingsBtn = document.getElementById("settings-btn");
+const settings = document.getElementById("settings");
+const settingsForm = document.getElementById("settings-form");
+const difficultySelect = document.getElementById("difficulty");
+
+// List of words for game
+const words = [
+  "sigh",
+  "tense",
+  "airplane",
+  "ball",
+  "pies",
+  "juice",
+  "warlike",
+  "bad",
+  "north",
+  "dependent",
+  "steer",
+  "silver",
+  "highfalutin",
+  "superficial",
+  "quince",
+  "eight",
+  "feeble",
+  "admit",
+  "drag",
+  "loving",
+  "height",
+  "theme",
+  "offshore",
+  "and",
+  "anchor",
+  "to",
+  "inflammable",
+  "is",
+  "you",
+  "that",
+  "it",
+  "he",
+  "was",
+  "for",
+  "on",
+  "are",
+  "as",
+  "with",
+  "his",
+  "they",
+  "india",
+  "at",
+  "be",
+  "this",
+  "have",
+  "from",
+  "or",
+  "one",
+  "had",
+  "by",
+  "word",
+  "but",
+  "not",
+  "what",
+  "all",
+  "were",
+  "we",
+  "when",
+  "your",
+  "can",
+  "said",
+  "there",
+  "use",
+  "an",
+  "each",
+  "which",
+  "she",
+  "do",
+  "how",
+  "their",
+  "if",
+  "will",
+  "up",
+  "other",
+  "about",
+  "out",
+  "many",
+  "then",
+  "them",
+  "these",
+  "solar",
+  "some",
+  "her",
+  "would",
+  "make",
+  "like",
+  "him",
+  "into",
+  "time",
+  "has",
+  "look",
+  "two",
+  "more",
+  "write",
+  "gotham",
+  "see",
+  "number",
+  "no",
+  "way",
+  "could",
+  "people",
+  "my",
+  "than",
+  "firstborn",
+  "water",
+  "been",
+  "callertune",
+  "who",
+  "oil",
+  "its",
+  "now",
+  "find",
+  "long",
+  "downtown",
+  "day",
+  "did",
+  "get",
+  "come",
+  "made",
+  "maintain",
+  "partial",
+];
+
+// Init word
+let randomWord;
+
+// Init score
+let score = 0;
+
+// Init time
+let time = 10;
+
+// Set difficulty to value in ls or medium
+let difficulty =
+  localStorage.getItem("difficulty") !== null
+    ? localStorage.getItem("difficulty")
+    : "medium";
+
+// Set difficulty select value
+difficultySelect.value =
+  localStorage.getItem("difficulty") !== null
+    ? localStorage.getItem("difficulty")
+    : "medium";
+
+// Start counting down
+const timeInterval = setInterval(updateTime, 1000);
+
+// Generate random word from array
+function getRandomWord() {
+  return words[Math.floor(Math.random() * words.length)];
+}
+
+// Add word to DOM
+function addWordToDOM() {
+  randomWord = getRandomWord();
+  word.innerHTML = randomWord;
+}
+
+// Update score
+function updateScore() {
+  score++;
+  scoreEl.innerHTML = score;
+}
+
+// Update time
+function updateTime() {
+  time--;
+  timeEl.innerHTML = time + "s";
+
+  if (time === 0) {
+    clearInterval(timeInterval);
+    // end game
+    gameOver();
+  }
+}
+
+// Game over, show end screen
+function gameOver() {
+  endgameEl.innerHTML = `
+    <h1>Time ran out</h1>
+    <p>Your final score is ${score}</p>
+    <button onclick="location.reload()">Reload</button>
+  `;
+
+  endgameEl.style.display = "flex";
+}
+
+addWordToDOM();
+
+// Event listeners
+
+// Typing
+text.addEventListener("input", (e) => {
+  const insertedText = e.target.value;
+
+  if (insertedText === randomWord) {
+    addWordToDOM();
+    updateScore();
+
+    // Clear
+    e.target.value = "";
+
+    if (difficulty === "hard") {
+      time += 1;
+    } else if (difficulty === "medium") {
+      time += 2;
+    } else {
+      time += 3;
+    }
+
+    updateTime();
+  }
+});
+
+// Settings btn click
+settingsBtn.addEventListener("click", () => settings.classList.toggle("hide"));
+
+// Settings select
+settingsForm.addEventListener("change", (e) => {
+  difficulty = e.target.value;
+  localStorage.setItem("difficulty", difficulty);
+});
